be7320b0321dfd23694e4228868d64dccfe4abdf
Marian <hellomariandev@gmail.com>
2022-08-20 23:20:15 +0200

    store basics

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
 
 const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Store
+export default Store;
