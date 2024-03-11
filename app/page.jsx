222c3c61416861c8517713816756478bed705e7b
Stupel <hellomariandev@gmail.com>
2024-03-11 14:47:26 +0100

    layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,30 @@
+// components
+import ProductCard from "@/components/product-card";
+import CartSummary from "@/components/cart-summary";
+import ItemCard from "@/components/item-card";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <h1 className="text-center text-2xl lg:text-4xl font-light">
-        Shopping Cart
-      </h1>
-    </main>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 w-full">
+      <div className="md:col-span-2 lg:col-span-3 bg-slate-50">
+        <h2 className="font-bold text-center py-4 text-xl">Store</h2>
+        {/* List of Products */}
+        <div className="flex flex-wrap justify-center">
+          <ProductCard />
+        </div>
+      </div>
+
+      <div className="sticky top-0 h-screen overflow-y-auto bg-slate-100 flex flex-col justify-between">
+        {/* Summary of Cart */}
+        <CartSummary />
+
+        <div className="flex-grow">
+          {/* Items in Cart */}
+          <div className="flex flex-col gap-y-2 justify-center">
+            <ItemCard />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
