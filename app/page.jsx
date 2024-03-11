4a72474b5d15706c8f2c8a0e2eae3fc8284b2e79
Stupel <hellomariandev@gmail.com>
2024-03-11 16:48:57 +0100

    basic functions

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,8 @@
+"use client";
+
+// react hook
+import { useState } from "react";
+
 // components
 import ProductCard from "@/components/product-card";
 import CartSummary from "@/components/cart-summary";
@@ -6,7 +11,53 @@ import ItemCard from "@/components/item-card";
 // mock data
 import data from "../data.json";
 
+// initial empty cart
+const defaultCart = [];
+
 export default function Home() {
+  // state of shopping cart
+  const [cart, setCart] = useState(defaultCart);
+
+  // get quantity of particular item
+  function itemQuantity(product) {
+    const id = product.id;
+    // find item based on ID and get quantity or return 0
+    return cart.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  // increment items quantity
+  function incrementItem(product) {
+    const id = product.id;
+  }
+
+  // decrement items quantity
+  function decrementItem(product) {
+    const id = product.id;
+  }
+
+  // remove item from cart
+  function removeItem(product) {
+    const id = product.id;
+    // filter items in cart based on ID
+    setCart((currItems) => currItems.filter((item) => item.id !== id));
+  }
+
+  // set cart to default state
+  function clearCart() {
+    setCart(defaultCart);
+  }
+
+  // get number of items in cart
+  const cartCount = cart.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  // get total price of items in cart
+  const totalPrice = cart.reduce(
+    (total, item) => item.price * item.quantity + total,
+    0
+  );
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 w-full">
@@ -22,7 +73,7 @@ export default function Home() {
 
       <div className="sticky top-0 h-screen overflow-y-auto bg-slate-100 flex flex-col justify-between">
         {/* Summary of Cart */}
-        <CartSummary />
+        <CartSummary cartCount={cartCount} totalPrice={totalPrice} />
 
         <div className="flex-grow">
           {/* Items in Cart */}
