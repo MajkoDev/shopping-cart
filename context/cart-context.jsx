0f6faccc6ff4ede688a322f27a2bc7ed86a8ceac
Stupel <hellomariandev@gmail.com>
2024-03-18 15:20:29 +0100

    finished

--- context/cart-context.jsx
+++ context/cart-context.jsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
+import useLocalStorage from "@/hooks/use-local-storage";
 
 const defaultCart = [];
 
 export const CartContext = createContext(null);
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState(defaultCart);
+  const [cart, setCart] = useLocalStorage('shop',defaultCart);
 
   function itemQuantity(id) {
     return cart.find((product) => product.id === id)?.quantity || 0;
